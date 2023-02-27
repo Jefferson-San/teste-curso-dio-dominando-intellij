0afb0454b711d6ebb7be86e3279cc55cf5a59738
Jefferson Santos <jeffsan.dev@gmail.com>
2023-02-27 15:41:12 -0300

    creating Book class

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -9,5 +9,40 @@ public class PrimeiroPrograma {
         int a = 4;
         int b = 3;
         System.out.println("Hello world " + (a + b));
+        Book book = new Book("O problema dos 3 corpos",300);
+        System.out.println(book);
+    }
+}
+class Book {
+    private String name;
+    private Integer numPg;
+
+    public Book(String name, Integer numPg) {
+        this.name = name;
+        this.numPg = numPg;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getNumPg() {
+        return numPg;
+    }
+
+    public void setNumPg(Integer numPg) {
+        this.numPg = numPg;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+            "name='" + name + '\'' +
+            ", numPg=" + numPg +
+            '}';
     }
 }
