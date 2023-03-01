68dc80af3a01e445b29f535ae796ad1778ceb884
Jefferson Santos <jeffsan.dev@gmail.com>
2023-02-28 23:30:07 -0300

    estudando sobre casting de variaveis do tipo number

--- src/br/com/dio/estudos/TiposVariaveis.java
+++ src/br/com/dio/estudos/TiposVariaveis.java
@@ -1,8 +1,21 @@
 package br.com.dio.estudos;
 
 public class TiposVariaveis {
+    /*Variaveis do tipo number:
+        inteiros:
+            byte, short, int, long.
+        fracionados:
+            float, double.
+    Existe casting de variáveis do tipo number:
+        O casting de variaveis de um tipo menor para o maior acontece de maneira
+        automática;
+        Já o casting de variaveis de um tipo maior para o menor deve ser feira de
+        forma manual:
+            ex: */
     public static void main(String[] args) {
-
-
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
     }
+
 }
